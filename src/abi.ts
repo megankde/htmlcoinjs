f48d7e11e4bd4221c6e7c147d4b4eac91b8d0e8a
denuoweb <jaron.rosenau@gmail.com>
2018-05-09 16:16:39 -0700

    update Htmlcoin

--- src/abi.ts
+++ src/abi.ts
@@ -10,13 +10,13 @@ const {
   encodeMethod,
   logDecoder,
   configure: configureABI,
-} = require("qtumjs-ethjs-abi") as IETHABI
+} = require("htmlcoinjs-ethjs-abi") as IETHABI
 
 configureABI({ noHexStringPrefix: true })
 
 import {
   ITransactionLog,
-} from "./QtumRPC"
+} from "./HtmlcoinRPC"
 
 export function encodeInputs(method: IABIMethod, args: any[] = []): string {
   const calldata = encodeMethod(method, args)
