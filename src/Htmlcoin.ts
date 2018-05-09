ca3df854c034813d1baec622f163e1f7db5b1f39
denuoweb <jaron.rosenau@gmail.com>
2018-05-09 16:05:08 -0700

    switch class Qtum to Htmlcoin

--- src/Htmlcoin.ts
+++ src/Htmlcoin.ts
@@ -1,14 +1,14 @@
-import { QtumRPC } from "./QtumRPC"
+import { HtmlcoinRPC } from "./HtmlcoinRPC"
 import { IContractsRepoData, ContractsRepo } from "./ContractsRepo"
 import { Contract } from "./Contract"
 
 /**
- * The `Qtum` class is an instance of the `qtumjs` API.
+ * The `Htmlcoin` class is an instance of the `htmlcoinjs` API.
  *
  * @param providerURL URL of the qtumd RPC service.
  * @param repoData Information about Solidity contracts.
  */
-export class Qtum extends QtumRPC {
+export class Htmlcoin extends QtumRPC {
   private repo: ContractsRepo
 
   constructor(providerURL: string, repoData?: IContractsRepoData) {
